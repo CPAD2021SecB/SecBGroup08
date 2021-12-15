5fb5d955e77accf7f37766229ea6dea85a773b48
Mohammed <salman.mohammed01@sap.com>
2021-12-15 21:45:49 +0530

    Updating the news url

--- vaarta/lib/strings.dart
+++ vaarta/lib/strings.dart
@@ -1,4 +1,4 @@
  class Strings {
-   static String newsurl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=API_KEY";
+   static String newsurl = "https://newsapi.org/v2/top-headlines?language=en&apiKey=API_KEY&sources=";
    static String sourceurl = "https://newsapi.org/v2/sources?language=en&apiKey=API_KEY";
  }
